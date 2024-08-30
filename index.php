30b9a710121ee78d62420173fdcc1c3c276a6395
Joshua <ojoshuacg@gmail.com>
2024-08-30 12:22:22 -0600

    Agregando nuevo contenido a la pagina principal

--- index.php
+++ index.php
@@ -16,6 +16,8 @@ require_once(__DIR__ . '/variables.php');
     <input type="submit" value="Descargar Excel">
 </form>
 
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi vero perferendis voluptas iusto, blanditiis at in, maxime debitis reiciendis, vel a harum distinctio expedita recusandae aliquid accusantium deleniti doloribus asperiores.</p>
+
 <?php
 require_once(__DIR__ . '/layout/footer.html')
 ?>
