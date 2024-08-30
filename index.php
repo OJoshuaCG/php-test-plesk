f09a9ab98941f19fff52239cf67ce89be962a320
Joshua <ojoshuacg@gmail.com>
2024-08-30 11:15:07 -0600

    feat. Cambiando variable de entorno por variable del servidor

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ require_once(__DIR__ . '/layout/head.html');
 require_once(__DIR__ . '/variables.php');
 ?>
 <h1 class="bg-success mx-auto p-2">
-    Hello <?= $_ENV['NOMBRE'] ?>
+    Hello <?= $_SERVER['NOMBRE'] ?>
 </h1>
 
 <div class="mx-auto p-2">
     <p class="">
-        Tu edad es <?= $_ENV['EDAD'] ?>
+        Tu edad es <?= $_SERVER['EDAD'] ?>
     </p>
 </div>
 <?php
