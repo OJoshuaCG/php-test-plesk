a9cf5d7c17f30ef47f94fafebaff420ef8cbfc95
Joshua <ojoshuacg@gmail.com>
2024-08-30 11:30:48 -0600

    Revert "feat. Cambiando variable de entorno por variable del servidor"
    This reverts commit f09a9ab98941f19fff52239cf67ce89be962a320.

--- index.php
+++ index.php
@@ -3,12 +3,12 @@ require_once(__DIR__ . '/layout/head.html');
 require_once(__DIR__ . '/variables.php');
 ?>
 <h1 class="bg-success mx-auto p-2">
-    Hello <?= $_SERVER['NOMBRE'] ?>
+    Hello <?= $_ENV['NOMBRE'] ?>
 </h1>
 
 <div class="mx-auto p-2">
     <p class="">
-        Tu edad es <?= $_SERVER['EDAD'] ?>
+        Tu edad es <?= $_ENV['EDAD'] ?>
     </p>
 </div>
 <?php
